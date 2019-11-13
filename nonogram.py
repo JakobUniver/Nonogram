9d4e93ecfc4ce2c0f270c8275e62f104c2d72d28
Jakob Univer <juniver@ut.ee>
2019-11-13 11:42:14 +0200

    Kindlad read paneb juba pildile

--- nonogram.py
+++ nonogram.py
@@ -26,10 +26,50 @@ def loe_nonogram_sisse(failinimi):
                         abijärjend.append(int(täht))
                 nonogram[1].append(abijärjend)
     return nonogram
+
 pilt=[]
+
 for rida in range(len(loe_nonogram_sisse("algne.txt")[1])):
     pilt.append([])
     for veerg in range(len(loe_nonogram_sisse("algne.txt")[0])):
         pilt[rida].append(" ")
+        
+read=loe_nonogram_sisse("algne.txt")[1]
+veerud=loe_nonogram_sisse("algne.txt")[0]
+
+sõne=""
+
+for rida in range(len(pilt)):
+    if type(read[rida])==int and read[rida]==len(pilt[rida]):
+        for element in read[rida]:
+            sõne+=element*"*"+" "
+        sõne=sõne[0:-1]
+        for indeks in range(len(sõne)):
+            pilt[rida][indeks]=sõne[indeks]
+        sõne=""
+    elif type(read[rida])==list and sum(read[rida])+len(read[rida])-1==len(pilt[rida]):
+        for element in read[rida]:
+            sõne+=element*"*"+" "
+        sõne=sõne[0:-1]
+        for indeks in range(len(sõne)):
+            pilt[rida][indeks]=sõne[indeks]
+        sõne=""
+
+for veerg in range(len(pilt[0])):
+    if type(veerud[veerg])==int and veerud[veerg]==len(pilt[veerg]):
+        for element in veerud[veerg]:
+            sõne+=element*"*"+" "
+        sõne=sõne[0:-1]
+        for indeks in range(len(sõne)):
+            pilt[indeks][veerg]=sõne[indeks]
+        sõne=""
+    elif type(veerud[veerg])==list and sum(veerud[veerg])+len(veerud[veerg])-1==len(pilt[veerg]):
+        for element in veerud[veerg]:
+            sõne+=element*"*"+" "
+        sõne=sõne[0:-1]
+        for indeks in range(len(sõne)):
+            pilt[indeks][veerg]=sõne[indeks]
+        sõne=""
+
 for rida in pilt:
     print(rida)
